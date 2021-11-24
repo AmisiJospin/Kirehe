4e50a0e7adda002b94baa0d8f1255d931c9720ef
Amisi Jospin Hassan <40860198+AmisiJospin@users.noreply.github.com>
2021-11-24 15:08:00 +0200

    I add a comment on the file

--- description.php
+++ description.php
@@ -1,61 +1,67 @@
+/*
+Name: Jospin Amisi
+Location: Lilongwe, Malawi
+Phone Number: +265992129078
+*/
+
 <?php
-		if(isset($_POST['boutonenvoyer']))
+	if(isset($_POST['boutonenvoyer']))
+	{
+		$compteur = 0;
+		$erreur = "";
+		if (empty($_POST['titre']))
 		{
-			$compteur = 0;
-			$erreur = "";
-			if (empty($_POST['titre']))
-			{
-				$compteur++;
-				$erreur .= $compteur .". Votre titre est incorrect <br/>";
-			}
-			else
-			{
-				$titre = $_POST['titre'];
-			}
-			
-			if (empty($_FILES['photo']['tmp_name']))
-			{
-				$compteur++;
-				$erreur .=$compteur .". Choississez une photo s.v.p!! ";
-			}
-			else
-			{
-				$destination = 'images/';
-				move_uploaded_file($_FILES['photo']['tmp_name'], $destination.$_FILES['photo']['name']);
-				$photo = $_FILES['photo']['name'];
-			}
-			
-			if (empty($_POST['texte']))
-			{
-				$compteur++;
-				$erreur .=$compteur .". Le texte n'est pas correct";
-			}
-			else
-			{
-				$texte = $_POST['texte'];
-			}
-			
-			
-		
-		if($compteur == 0){
-		
-				include('connexion.php'); //connexion à la base de données
-				
-				$date_soumis = time();
-				
-				$requete = $bdd -> prepare('INSERT INTO article (titre, photo,texte ) VALUES(?,?,?)');
-				
-				$requete -> execute(array($titre, $photo, $texte));
-				
-			echo '<p>Votre Titre: '.$titre .'</p>';
-			echo '<p>Votre Photo est: <br>'.$photo .'</p>';
-			echo '<p>Votre Texte: '.$texte .'</p>';
-			}
-		
+			$compteur++;
+			$erreur .= $compteur .". Votre titre est incorrect <br/>";
+		}
 		else
-			{
-			echo 'Corrigez les erreurs suivants:<br/>'.$erreur;
-			}
+		{
+			$titre = $_POST['titre'];
+		}
+
+		if (empty($_FILES['photo']['tmp_name']))
+		{
+			$compteur++;
+			$erreur .=$compteur .". Choississez une photo s.v.p!! ";
+		}
+		else
+		{
+			$destination = 'images/';
+			move_uploaded_file($_FILES['photo']['tmp_name'], $destination.$_FILES['photo']['name']);
+			$photo = $_FILES['photo']['name'];
+		}
+
+		if (empty($_POST['texte']))
+		{
+			$compteur++;
+			$erreur .=$compteur .". Le texte n'est pas correct";
+		}
+		else
+		{
+			$texte = $_POST['texte'];
+		}
+
+
+
+	if($compteur == 0){
+
+			include('connexion.php'); //connexion à la base de données
+
+			$date_soumis = time();
+
+			$requete = $bdd -> prepare('INSERT INTO article (titre, photo,texte ) VALUES(?,?,?)');
+
+			$requete -> execute(array($titre, $photo, $texte));
+
+		echo '<p>Votre Titre: '.$titre .'</p>';
+		echo '<p>Votre Photo est: <br>'.$photo .'</p>';
+		echo '<p>Votre Texte: '.$texte .'</p>';
+		}
+
+	else
+		{
+		echo 'Corrigez les erreurs suivants:<br/>'.$erreur;
+		}
 
 		
 }
